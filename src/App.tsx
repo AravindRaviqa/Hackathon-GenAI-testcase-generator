20bf3c347d929e1a34079b871bed1295c8db4a78
AravindRaviqa <aravind.ravi@focaloid.com>
2025-06-30 13:17:19 +0530

    Create missing index.html and React entry files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Hackathon GenAI Testcase Generator</h1>
+      <p>Welcome to the GenAI testcase generator!</p>
+    </div>
+  )
+}
+
+export default App
